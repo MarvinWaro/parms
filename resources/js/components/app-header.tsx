7ee1dcaaba1e78639e9c5d1c30bef70d94fa100c
MarvinWaro <mrvnwr.dev@gmail.com>
2025-08-15 07:58:23 +0800

    Update nav and heading styles in app and pages
    Added 'Properties' to main navigation with a Laptop icon in app-header. Changed heading tags from h1 to h5 in condition and location index pages for improved hierarchy and consistency.

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -12,7 +12,7 @@ import { useAppearance } from '@/hooks/use-appearance';
 import { cn } from '@/lib/utils';
 import { type BreadcrumbItem, type NavItem, type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { LayoutGrid, MapPinCheckInside, Menu, Moon, Sun, ScanHeart, Monitor } from 'lucide-react';
+import { LayoutGrid, MapPinCheckInside, Menu, Moon, Sun, ScanHeart, Monitor, Laptop } from 'lucide-react';
 import AppLogo from './app-logo';
 import AppLogoIcon from './app-logo-icon';
 
@@ -31,6 +31,11 @@ const mainNavItems: NavItem[] = [
         title: 'Conditions',
         href: '/condition',
         icon: ScanHeart,
+    },
+    {
+        title: 'Properties',
+        href: '/property',
+        icon: Laptop,
     },
 ];
 
